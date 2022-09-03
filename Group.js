15fdf445ff9f858a6a3ae2ff5855532dba508257
Your Name <you@example.com>
2022-09-03 09:45:38 -0400

    separated group class from groupIterator

--- Group.js
+++ Group.js
@@ -74,27 +74,3 @@ let gp = Group.from([50, 80]);
 console.log(gp);
 gp.has2(60);
 gp.has2(30);
-
-class GroupIterator {
-  constructor(group) {
-    this.group = group;
-    this.position = 0;
-  }
-
-  next() {
-    if (this.position >= this.group.members.length) {
-      return { done: true };
-    } else {
-      let result = {
-        value: this.group.members[this.position],
-        done: false,
-      };
-      this.position++;
-      return result;
-    }
-  }
-}
-
-for (let value of Group.from(["a", "b", "c"])) {
-  console.log(value);
-}
--- GroupIterator.js
+++ GroupIterator.js
@@ -17,3 +17,15 @@ class GroupIterator {
     }
   }
 }
+
+Group.prototype[Symbol.iterator] = function () {
+  return new GroupIterator(this);
+};
+
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
+
+//a
+// b
+//c
