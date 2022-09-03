93f286a4d2c584b8eebf404488149055549c9b83
Your Name <you@example.com>
2022-09-03 09:18:37 -0400

    created an iterable class to loop thru

---
+++ GroupIterator.js
@@ -0,0 +1,19 @@
+class GroupIterator {
+  constructor(group) {
+    this.group = group;
+    this.position = 0;
+  }
+
+  next() {
+    if (this.position >= this.group.members.length) {
+      return { done: true };
+    } else {
+      let result = {
+        value: this.group.members[this.position],
+        done: false,
+      };
+      this.position++;
+      return result;
+    }
+  }
+}
